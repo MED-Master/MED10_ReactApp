e63bf5cd0d7319f7566cfb4779d364a3b45738e3
SteffenLehmann <slehma15@student.aau.dk>
2023-04-13 11:11:21 +0200

    Added progressbar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,65 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, FlatList, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, Button, FlatList, Image, StyleSheet, TouchableOpacity, Animated } from 'react-native';
+//import * as Progress from 'react-native-progress';
+//import { StatusBar } from 'status-bar';
 //import store from './Store/messages';
+const Progress =({step, steps, height}) => {
+  const [width, setWidth] = React.useState(0);
+  const animatedValue = React.useRef(new Animated.Value(-1000)).current;
+  const reactive = React.useRef(new Animated.Value(-1000)).current;
+  
+
+  React.useEffect(() => {
+    Animated.timing(animatedValue, {
+      toValue: reactive,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+  
+  React.useEffect(() => {
+    reactive.setValue(-width + (width * step) / steps);
+  }, [step,width]);
+
+  return (
+    <>
+    <Text style={{
+      fontFamily: 'menlo',
+      fontSize: 12,
+      fontWeight: '800',
+      marginBottom: 2,
+    }}>
+      {step}/{steps}
+    </Text>
+    <View 
+      onLayout={(e) => {
+        const newWidth = e.nativeEvent.layout.width;
+
+        setWidth(newWidth);
+      }}
+      style={{
+        height,
+        backgroundColor: 'white',
+        borderRadius: height,
+        overflow: 'hidden',
+      }}>
+      <Animated.View 
+        style={{
+          height,
+          width: '100%',
+          backgroundColor: '#F9CA7F',
+          borderRadius: height,
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          transform: [{ translateX: animatedValue }],
+        }} />
+    </View>
+    </>
+  ) 
+  
+}
+
 
 const ChatScreen = () => {
   const [messages, setMessages] = useState([
@@ -42,28 +101,42 @@ const ChatScreen = () => {
   };
 
   const answers = [
-    "Strongly agree",
-    "Moderately agree",
-    "Neither agree nor disagree",
-    "Moderately disagree",
-    "Strongly disagree"
+    "Meget enig",
+    "Moderat enig",
+    "Hverken enig eller uenig",
+    "Moderat uenig",
+    "Meget uenig"
   ];
   //TODO use styles
-  
+  //<Progress.Bar progress={0.3} width={200} />
+  const [indexBar, setIndex] = React.useState(0);
+  React.useEffect(() => {
+    const interval = setInterval(() =>{
+      setIndex((indexBar + 1) % (49 + 1))
+    }, 10)
+
+    return () => {
+      clearInterval(interval);
+    }
+  },[indexBar])
+
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
-        <Text style={styles.question}>Question very important????</Text>
-        
+        <Text style={styles.question}>Havde du problemer med at tale?Havde du problemer med at tale?</Text>
+        <View style={styles.progressContainer}>
+          <Progress step={12} steps={49} height={10} />
+        </View>
         <FlatList
         data={answers}
-        style={styles.ratings}
+        horizontal = {true}
         renderItem={({ item }) => (
-          <TouchableOpacity style={styles.sendButtonStyle} onPress={handleSend}>
-          <Text style={styles.textSettingsButton}>{item}</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.likertButtonStyle} onPress={handleSend}>
+            <Text style={styles.textSettingsLikertButton}>{item}</Text>
+          </TouchableOpacity>
         )}
         keyExtractor={(item, index) => index.toString()}
+        
       />
         
       </View>
@@ -71,7 +144,7 @@ const ChatScreen = () => {
         data={messages}
         renderItem={({ item }) => (
           <View style={item.me ? styles.inputMessage : styles.botMessage}>
-            <Text>{ item.author + 'I am of breaddy nature, daaddy :'}</Text> 
+            <Text>{ item.author + ':'}</Text> 
             { item.image ? <Image source={{ uri: item.image }} style={{ width: 200, height: 200 }} /> : null }
             { item.text ?
               <Text style={{ fontWeight: 'bold', color: item.me ? 'black' : '#000' }}>{item.text}</Text> : null
@@ -80,6 +153,7 @@ const ChatScreen = () => {
         )}
         keyExtractor={(item, index) => index.toString()}
       />
+      <View style={{borderBottomColor: 'blue', borderBottomWidth: 2}}/>
       <View style={styles.textFieldsInputContainer}>
         <TextInput
           style={styles.textFieldsInput}
@@ -99,15 +173,18 @@ const DEFAULT_PADDING = 10;
 const styles = StyleSheet.create({ //design of the chat screen
   headerContainer: {
     backgroundColor: '#F7E5C9',
+    borderBottomEndRadius: 5,
+    borderBottomStartRadius: 5,
   },
   question: {
-    fontSize: 20,
+    fontSize: 26,
     padding: DEFAULT_PADDING,
     fontWeight: 'bold',
     textAlign: 'left',
+    color: 'black',
   },
   ratings: {
-    flexDirection: 'row',
+    //flexDirection: 'row',
   },
   ratingButton: {
     flex: 1,
@@ -122,11 +199,34 @@ const styles = StyleSheet.create({ //design of the chat screen
     backgroundColor: '#F9CA7F',
     padding: DEFAULT_PADDING,
     borderRadius: 10,
-    //fontWeight: 'bold',
+    //flex: 1,
+    width: 120,
+    height: 50,
+    textAlignVertical: 'center',
+  },
+  likertButtonStyle: { 
+    backgroundColor: '#F9CA7F',
+    borderRadius: 10,
+    maxWidth: 78,
+    maxHeight: 100,
+    minWidth: 78,
+    justifyContent: 'flex-start',
+    alignContent: 'center',
+    paddingVertical: 6,
+    paddingHorizontal: 6,
+    margin: 2,
+    
   },
   textSettingsButton: {
     fontWeight: 'bold',
     fontSize: 16,
+    color: 'black',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  textSettingsLikertButton: {
+    fontWeight: 'bold',
+    fontSize: 14,
     color: 'black',
     textAlign: 'center',
   },
@@ -161,6 +261,10 @@ const styles = StyleSheet.create({ //design of the chat screen
     borderRadius: 10,
     margin: 5,
     width: '90%',
+  },
+  progressContainer: {
+    width: '95%',
+    alignSelf: 'center',
   },
 })
 export default ChatScreen;
