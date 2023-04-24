32f26a67a31ad346945edca22f1ec23c8678a60c
VeRonyKaa <vnemco18@student.aau.dk>
2023-04-24 15:36:34 +0200

    Update App.tsx
    button margin fix

--- App.tsx
+++ App.tsx
@@ -98,7 +98,7 @@ const ChatScreen = () => {
     }
   }, [currentOption]);
 
-  const [progress, setProgress] = useState(26);
+  const [progress, setProgress] = useState(27);
 
   useEffect(() => {
     if(progress > 26) {
@@ -120,8 +120,8 @@ const ChatScreen = () => {
           <Progress step={progress} steps={49} height={10} />
         </View>
           <FlatList
-          //contentContainerStyle={{flexDirection : "row", flexWrap : "wrap", justifyContent: 'center', alignItems: 'center'}} 
-          horizontal = {true}
+          contentContainerStyle={{flexDirection : "row", flexWrap : "wrap", justifyContent: 'center', alignItems: 'center'}} 
+          //horizontal = {true}
           data={enumerate(currentOptionList)}
           //numColumns={5}
           renderItem={({ item }) => (
@@ -212,8 +212,8 @@ const styles = StyleSheet.create({ //design of the chat screen
   likertButtonStyle: { 
     backgroundColor: '#F9CA7F',
     borderRadius: 10,
-    maxWidth: 78,
-    minWidth: 78,
+    maxWidth: 72,
+    minWidth: 72,
     maxHeight: 70,
     minHeight: 70,
     paddingVertical: 6,
@@ -226,8 +226,8 @@ const styles = StyleSheet.create({ //design of the chat screen
   likertButtonStylePressed: { 
     backgroundColor: '#F4B34B',
     borderRadius: 10,
-    maxWidth: 78,
-    minWidth: 78,
+    maxWidth: 72,
+    minWidth: 72,
     maxHeight: 70,
     minHeight: 70,
     paddingVertical: 6,
